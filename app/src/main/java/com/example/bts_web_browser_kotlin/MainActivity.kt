7380a82787f380d8395a54bf91c5aa00f5fcaf46
lucasoskorep <lucas.oskorep@gmail.com>
2020-10-19 14:13:47 -0500

    Adding vars to the MainActivity

--- app/src/main/java/com/example/bts_web_browser_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/bts_web_browser_kotlin/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.bts_web_browser_kotlin
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var webview:WebView
+    lateinit var searchButton:ImageButton
+    lateinit var backButton:ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+    class WebClient internal constructor(private val activity: Activity): WebViewClient(){
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            view?.loadUrl(request?.url.toString())
+            return true
+        }
+
     }
 }
