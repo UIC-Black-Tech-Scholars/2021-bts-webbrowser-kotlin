7d9e7c99adc3c63e5ff0015f9f62cee458a46fcb
lucasoskorep <lucas.oskorep@gmail.com>
2020-10-19 14:03:58 -0500

    Adding logic to finalized branch

--- app/src/main/java/com/example/bts_web_browser_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/bts_web_browser_kotlin/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.bts_web_browser_kotlin
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    class WebClient internal constructor(private val activity: Activity): WebViewClient(){
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            view?.loadUrl(request?.url.toString())
+            return true
+        }
+
     }
 }
